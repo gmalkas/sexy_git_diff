ef54e02a09df3de198687d0146ec9fca3b779707
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-02-04 21:05:34 +0100

    Fix param names

--- sexy_diff.rb
+++ sexy_diff.rb
@@ -16,8 +16,8 @@ get '/' do
 		
 		@repo = Grit::Repo.new(repository)
 
-		@from_sha = params[:from] || @repo.commits.last.sha
-		@to_sha = params[:to] || @repo.commits.first.sha
+		@from_sha = params[:from_commit] || @repo.commits.last.sha
+		@to_sha = params[:to_commit] || @repo.commits.first.sha
 		@diffs = @repo.diff(@from_sha, @to_sha, *files)
 		@diff_presenters = @diffs.map { |d| DiffPresenter.new(d.diff) }
   end
